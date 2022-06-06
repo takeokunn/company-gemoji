2aa0830329c71dbff8068559afbff0caa656313a
takeokunn <bararararatty@gmail.com>
2022-06-06 13:42:18 +0900

    Fix

--- generate.rb
+++ generate.rb
@@ -9,14 +9,14 @@ jisyo_path = File.join(__dir__, 'company-gemoji-data.el')
 File.open(jisyo_path, 'w') do |f|
   f << ";;; company-gemoji-data.el --- Terraform documentation as elisp lists and hashes\n"
   f << "\n"
-  f << ";;; THIS FILE WAS AUTOMATICALLY GENERATED. DO NOT EDIT.\n"[O]
+  f << ";;; THIS FILE WAS AUTOMATICALLY GENERATED. DO NOT EDIT.\n"
   f << "\n"
   f << ";;; Code:\n"
   f << "\n"
   f << "(defconst company-gemoji-data-keywords\n"
   f << "  \'(\n"
   names.each do |name|
-    f << "    \"#{name}\"\n"
+    f << "    \":#{name}:\"\n"
   end
   f << "    )\n"
   f << "  \"Keywords of the gemoji.\")\n"
